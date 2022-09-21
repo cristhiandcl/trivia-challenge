a7abe6a0e11dceaa870afbcca2a179b62d959df8
Cristhian David Carrillo Lopez <cristhiandcl8@MacBook-Pro-de-Cristhian.local>
2022-09-21 15:46:49 -0500

    update

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,7 @@ function Results({ result, score, questionsLength }) {
   return (
     <div className="h-screen  w-2/4 flex flex-col justify-center mx-auto space-y-4">
       <p className="font-bold text-2xl text-center pb-8">
-        You score {`${score} / ${questionsLength}`}
+        You scored {`${score} / ${questionsLength}`}
       </p>
       {result.map((elem, index) => (
         <p key={index}>{elem}</p>
